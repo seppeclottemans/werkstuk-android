0a2e066ecc40fbd863d1a45a6dc75d24b0a01daa
seppe <seppe.clottemans@student.ehb.be>
2020-05-19 16:24:05 +0200

    fall detection code

--- Werkstuk/app/src/main/java/com/example/werkstuk/MainActivity.java
+++ Werkstuk/app/src/main/java/com/example/werkstuk/MainActivity.java
@@ -11,11 +11,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "MainActivity";
 
-    // watched this tutorial for the basic uses of the accelerometer (logging the results on change): https://www.youtube.com/watch?v=Rda_5s4rObQ
+    // watched this tutorial for the basic uses of the accelerometer (logging the results on change): https://www.youtube.com/watch?v=Rda_5s4rObQ (19/05/2020)
     private SensorManager sensorManager;
     Sensor accelerometer;
 
@@ -37,6 +39,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+
+        // fall detection code from: https://stackoverflow.com/questions/44518729/android-fall-detection (19/05/2020)
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+
+            double loX = sensorEvent.values[0];
+            double loY = sensorEvent.values[1];
+            double loZ = sensorEvent.values[2];
+
+            double loAccelerationReader = Math.sqrt(Math.pow(loX, 2)
+                    + Math.pow(loY, 2)
+                    + Math.pow(loZ, 2));
+
+            DecimalFormat precision = new DecimalFormat("0.00");
+            double ldAccRound = Double.parseDouble(precision.format(loAccelerationReader));
+
+            // for testing in the emulator: loAccelerationReader <= 6.0
+            // for fall accuracy: ldAccRound > 0.3d && ldAccRound < 0.5d
+            if (loAccelerationReader <= 6.0) {
+                Log.d(TAG, "fall detected!");
+                Toast.makeText(this, "Fall detected", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
