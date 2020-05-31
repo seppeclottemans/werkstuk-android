d9f0651b712896e8214abafc378845abd089aeba
seppe <seppe.clottemans@student.ehb.be>
2020-05-31 05:05:09 +0200

    about me page + custom checkbox

--- Werkstuk/app/src/main/java/com/example/werkstuk/ui/about_me/AboutMeFragment.java
+++ Werkstuk/app/src/main/java/com/example/werkstuk/ui/about_me/AboutMeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,12 +14,24 @@ import com.example.werkstuk.R;
 
 public class AboutMeFragment extends Fragment {
 
-    private AboutMeViewModel aboutMeViewModel;
+    private CheckBox customCheckBox;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.about_me, container, false);
+        final View root = inflater.inflate(R.layout.about_me, container, false);
+
+        customCheckBox = root.findViewById(R.id.custom_checkbox);
+        customCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(customCheckBox.isChecked()){
+                    Toast.makeText(getContext(), "Thank You!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         return root;
     }
+
+
 }
--- Werkstuk/app/src/main/java/com/example/werkstuk/ui/about_me/AboutMeViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.werkstuk.ui.about_me;
-
-import androidx.lifecycle.ViewModel;
-
-public class AboutMeViewModel extends ViewModel {
-
-}
